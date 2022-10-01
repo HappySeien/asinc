50fceb284e8d532a6647aadcb82036714b34b9a8
Vladislav Goryakin <Happy.Seien@yandex.ru>
2022-10-01 11:00:07 +0300

    s-4 #3
    test #3

--- asinc_chat/tests/test_client.py
+++ asinc_chat/tests/test_client.py
@@ -1,4 +1,5 @@
 import unittest
+from server import Server
 from client import Client, DESC
 from services.parse_cli_arguments import parse_cli_arguments
 
@@ -8,12 +9,19 @@ class ClientTest(unittest.TestCase):
     Тесты клиента
     """
     args = parse_cli_arguments(DESC)
+    server_ = Server(host=args.host, port=args.port)
     client_ = Client(host=args.host, port=args.port)
     
     def setUp(self) -> None:
-        self.client_.run()
+        self.server_.run()
 
     def tearDown(self) -> None:
+        pass
+
+    def test_parse_response(self):
+        pass
+
+    def test_send_message(self):
         pass
 
 
--- asinc_chat/tests/test_server.py
+++ asinc_chat/tests/test_server.py
@@ -1,5 +1,6 @@
 import unittest
 from server import Server, DESC
+from client import Client
 from services.parse_cli_arguments import parse_cli_arguments
 
 
@@ -9,12 +10,19 @@ class TestServer(unittest.TestCase):
     """
     args = parse_cli_arguments(DESC)
     server_ = Server(host=args.host, port=args.port)
+    client_ = Client(host=args.host, port=args.port)
 
     def setUp(self) -> None:
         self.server_.run()
 
     def tearDown(self) -> None:
         self.server_.close()
+
+    def test_parse_message(self):
+        pass
+
+    def test_send_response(self):
+        pass
 
 
 
